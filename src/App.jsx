5d6add2a176ab324b2adecbeffeca5ce2746c3a6
François Jamet <francois.jamet@etu.minesparis.psl.eu>
2023-09-12 10:28:55 +0200

    Test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <div>
+      <p align="center"><img src="https://www.sncf-voyageurs.com/medias-publics/styles/original/public/2023-08/tgv-paysage-header.jpg.webp?itok=-F7TH_GO"></img></p>
       <p class="text-4xl text-green-700 text-center py-20">Hello les Mines !</p>
       <p class="text-4xl text-green-800 text-center py-20">Vous allez bien ?</p>
     </div>
