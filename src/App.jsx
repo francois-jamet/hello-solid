b48b65285c622088a6c570e309e8c5a7c4f1ecc0
François Jamet <francois.jamet@etu.minesparis.psl.eu>
2023-09-12 10:28:55 +0200

    Test++

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <p align="center"><img src="https://www.sncf-voyageurs.com/medias-publics/styles/original/public/2023-08/tgv-paysage-header.jpg.webp?itok=-F7TH_GO"></img></p>
       <p class="text-4xl text-green-700 text-center py-20">L'histoire du TGV de 1981 à nos jours !</p>
-      <p class="text-2xl text-green-800 text-center py-20"><l>La grande histoire du train</l></p>
+      <p class="text-2xl text-green-800 text-center py-20"><em>La grande histoire du train</em></p>
     </div>
   );
 }
