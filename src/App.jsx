b60752bb353cfde2cdba3b03e2239e18bcaf16d5
François Jamet <francois.jamet@etu.minesparis.psl.eu>
2023-09-12 10:28:55 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello les Mines !</p>
+    <div>
+      <p class="text-4xl text-green-700 text-center py-20">Hello les Mines !</p>
+      <p class="text-4xl text-green-800 text-center py-20">Vous allez bien ?</p>
+    </div>
   );
 }
 
